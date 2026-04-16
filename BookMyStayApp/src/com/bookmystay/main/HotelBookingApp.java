d58774a7f2b7922b8e031a17afc23e47837359fd
xxxASHxxx <theofficialashmit@gmail.com>
2026-04-16 20:34:12 +0530

    UC1 - Application Entry Point

---
+++ BookMyStayApp/src/com/bookmystay/main/HotelBookingApp.java
@@ -0,0 +1,13 @@
+package com.bookmystay.main;
+
+/**
+ * Main application entry point for Book My Stay.
+ *
+ * @author Ashmit
+ * @version 1.0
+ */
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay - Hotel Booking System v1.0");
+    }
+}
